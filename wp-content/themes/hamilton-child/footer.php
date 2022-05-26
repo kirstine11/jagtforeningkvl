5e25eb2a2ac062e1850a6ecfd4af1ba80b9198dc
juli741j <juli741j@stud.kea.dk>
2022-05-26 17:54:44 +0200

    ændringer

--- wp-content/themes/hamilton-child/footer.php
+++ wp-content/themes/hamilton-child/footer.php
@@ -5,25 +5,25 @@
 			<div class="row">
     
 <div class="column">
-<h4>KONTAKT</h4>
+<h3>KONTAKT</h3>
 <p> Jagtforeningen KVL</p>
 <p>Bestyrelsen@jagtkvl.dk</p>
 </div>
 
 <div class="column">
-<h4>FIND OS</h4>
+<h3>FIND OS</h3>
 <p>Dyrlægevej 7</p>
 	<p>1870 Frederiksberg</p>
 </div>
 
 <div class="column">
-<h4>INFORMATION</h4><a href="https://kirstinekrogs.dk/jagtforeningkvl/privatlivspolitik/"><p>Privatlivspolitik</p></a>
+<h3>INFORMATION</h3><a href="https://kirstinekrogs.dk/jagtforeningkvl/privatlivspolitik/"><p>Privatlivspolitik</p></a>
 <a href="https://kirstinekrogs.dk/jagtforeningkvl/cookiepolitik-eu/"><p>Cookiepolitik</p></a>
 <a href="https://kirstinekrogs.dk/jagtforeningkvl/refundering_returvarer/"><p>Handelsbetingelser</p></a>
 </div>
 
 <div class="nyhedsbrev">
-<h4>TILMELD DIG VORES NYHEDSBREV</h4>
+<h3>TILMELD DIG VORES NYHEDSBREV</h3>
 <p>
 	<label>
 		<input type="email" name="EMAIL" placeholder="Indtast e-mail" required />
