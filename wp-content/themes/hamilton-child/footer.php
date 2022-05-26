a38e143cbd1c31dfc381f46e576877936b173052
Lærke <laer579a@stud.kea.dk>
2022-05-25 20:02:05 -0700

    footer

--- wp-content/themes/hamilton-child/footer.php
+++ wp-content/themes/hamilton-child/footer.php
@@ -2,8 +2,32 @@
 		
 		<footer class="site-footer section-inner">
 
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
-			<p class="theme-by"><?php _e( 'Theme by', 'hamilton' ); ?> <a href="https://www.andersnoren.se">Anders Nor&eacute;n</a></p>
+			<div class="row">
+    
+<div class="column">
+<h3>KONTAKT</h3>
+<p> Jagtforeningen KVL
+Bestyrelsen@jagtkvl.dk</p>
+</div>
+
+<div class="column">
+<h3>FIND OS</h3>
+<p>Dyrlægevej 7
+1870 Frederiksberg 
+	</p>
+<a href="https://kirstinekrogs.dk/kea/Dinplanteven/cookie-policy-eu/"><p>Cookiepolitik</p></a>
+</div>
+
+<div class="social">
+<a href="https://www.facebook.com/Dinplantevenmmddk-106563078469156" class="facebook"><img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/fb.svg" alt="fb ikon"></a>
+<a href="https://www.instagram.com/dinplantevenmmd.dk/" class="instagram"><img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/Instagram.svg" alt="insta ikon"></a>
+</div>
+
+<div class="flower">
+<img src="https://kirstinekrogs.dk/kea/Dinplanteven/wp-content/uploads/2021/12/flowers.svg" alt="blomst">
+</div>
+
+</div>
 
 		</footer><!-- footer -->
 	    
